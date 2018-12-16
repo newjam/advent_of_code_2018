3b218f68307abdef0d206679566c69f5f3b50abc
James Newman <jameshildrethnewman@gmail.com>
2018-12-16 06:44:54 +0000

    part 2 day 12

--- day_12/solution.hs
+++ day_12/solution.hs
@@ -36,6 +36,17 @@ part1 n xs ps = sum generation
     generations = iterate (next ps) xs
     generation  = generations !! n
 
+-- Part 2 requires finding the answer for the 50000000000th generation.
+-- This is intractible to simulate, so some analysis is in order.
+-- Mustering my first quarter of undergraduate math, we can find the first difference, and second difference
+-- given our generations,
+-- let sums = map sum generations
+-- let diff1 = zipWith (-) (drop 1 sums) sums
+-- let diff2 = zipWith (-) (drop 1 diff1) diff1
+-- Then we can see that diff2 !! n == 0 for all n >= 108.
+-- Also, sums !! 108 == 7976
+-- So the answer is 65 * (50000000000 - 108) + 7976, ie 3250000000956!
+
 draw nth xs ps = drawPots $ (iterate (next ps) xs) !! nth
 
 handle parseResult = case parseResult of
@@ -45,8 +56,11 @@ handle parseResult = case parseResult of
 expect (Right x) = x
 expect (Left e) = error . show $ e
 
-table n xs ps = sequence_ . take n .  map (putStrLn . show . sum) $ generations
+table n xs ps = sequence_ . take n .  map (putStrLn . ) $ [0..] generations
   where generations = iterate (next ps) xs
+        sums = map sum generations
+        diff1s = zipWith (-1) sums (drop 1 sums)
+        row (i, generation) = show i ++ " " ++ (show . sum $ generation)
 
 
 
